7f67240bcba1a27c8bffe2954a689b60d7e854cc
James McGeachie <jamesmcgeachie10@gmail.com>
2015-11-12 16:46:49 -0500

    User authentication added! Oh yeah!

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
 
   resources :products
-
-
-  # get 'products/index'
-
-  # get 'products/show'
-
-  # get 'products/new'
-
-  # get 'products/edit'
+  resources :users, only: [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
