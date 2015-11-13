f1cba436af8d2ac694e33c0df2cce83286f9f513
James McGeachie <jamesmcgeachie10@gmail.com>
2015-11-12 19:02:13 -0500

    added reviews model and relationships with users and products

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,8 @@ class Product < ActiveRecord::Base
 	validates :description, :name, presence: true
 	validates :price_in_cents, numericality: {only_integer: true}
 
+	has_many :reviews
+
 	def cents_to_dollars
 		price_in_dollars = price_in_cents.to_f / 100
 		sprintf("%.2f", price_in_dollars)
