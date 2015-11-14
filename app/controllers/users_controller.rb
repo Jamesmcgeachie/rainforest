92669587ef20b0a3aa6f191bb6694172860e060e
James McGeachie <jamesmcgeachie10@gmail.com>
2015-11-13 19:12:39 -0500

    Added name column to users table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,6 @@ class UsersController < ApplicationController
 
   private
   def sanitized_user_params
-  	params.require(:user).permit(:email, :password, :password_confirmation)
+  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 
+	validates :name, presence: true
 	has_secure_password
-
 	has_many :reviews
 
 end
