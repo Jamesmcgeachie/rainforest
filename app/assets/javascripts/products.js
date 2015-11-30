18aecf43cb38e0da9fa0ede46aa0bef4a6035c60
James McGeachie <jamesmcgeachie10@gmail.com>
2015-11-30 16:47:53 -0500

    Wrote jQuery for the search AJAX request

---
+++ app/assets/javascripts/products.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	$('#search-form').submit(function() {
+		event.preventDefault();
+		var searchString = $('#search').val();
+
+		$.ajax({
+			url: '/products?search=' + searchString;
+			type: 'GET'
+			dataType: 'html'
+		}).done(function(data){
+			$('#products').html(data);
+		})
+	})
+});
